5b6f253113259f697b7a68ae2f0e3532b6ff695a
Eugen Regner <eugen.regner@gmail.com>
2023-02-12 19:52:26 +0100

    add routes

--- src/pages/App.ts
+++ src/pages/App.ts
@@ -1,6 +1,28 @@
 import classes from './App.module.css'
 
+const routes = [
+  {
+    path: '/',
+    page: 'to-dos'
+  },
+  {
+    path: '/todos',
+    page: 'to-dos'
+  },
+  {
+    path: '/settings',
+    page: 'app-settings'
+  },
+]
+
+// function matchRoute (routes, requestedRoute) {
+//
+// }
+
 export class App extends HTMLElement {
+
+  #startLocation = window.location.pathname || '/'
+
   constructor () {
     super()
     this.render()
@@ -9,8 +31,22 @@ export class App extends HTMLElement {
   render () {
     const div = document.createElement('div')
     div.classList.add(classes.App)
-    div.textContent = 'APP'
+    div.insertAdjacentHTML('afterbegin', '<h1>Application</h1>')
     this.appendChild(div)
+    console.log('Render APP')
+  }
+
+  connectedCallback () {
+    window.addEventListener('popstate', this.navigate.bind(this))
+    window.dispatchEvent(new PopStateEvent('popstate', { state: { route: this.#startLocation } }))
+  }
+
+  disconnectedCallback () {
+    window.removeEventListener('popstate', this.navigate.bind(this))
+  }
+
+  navigate (event: PopStateEvent) {
+    console.log(event.state.route)
   }
 }
 
